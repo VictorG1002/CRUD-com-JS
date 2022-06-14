1f531e19d3757036a3d2c8c1d13e42fd509fba2b
Victor <vggaia10@gmail.com>
2022-06-13 23:04:10 -0300

    adiciona logica de salvar lembretes

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-const crud = {
-  posts: [
-    {
-      conteudo: 'aaaaaa'
-    }
-  ]
+let input = document.getElementById('input')
+let botao = document.getElementById('botao')
+let form = document.getElementById('form')
+let conteudo = document.getElementById('conteudo')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  console.log('botao clicado')
+
+  ValidaForm()
+})
+
+let ValidaForm = () => {
+  if (input.value != '') {
+    aceitaLembretes()
+  }
+}
+
+const lembretes = {}
+
+let aceitaLembretes = () => {
+  lembretes['text'] = input.value
+  console.log(lembretes)
 }
 
 function criaPost(mensagem) {
