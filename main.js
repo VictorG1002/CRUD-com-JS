fbfd5a0df86b21ba06f0a938bde6370aed828f46
Victor <vggaia10@gmail.com>
2022-06-21 00:17:42 -0300

    add editPost function

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let form = document.getElementById('form')
 let input = document.getElementById('input')
 let botao = document.getElementById('botao')
 let posts = document.getElementById('posts')
-let conteudo = document.getElementById('conteudo')
 
 form.addEventListener('submit', e => {
   e.preventDefault()
@@ -27,7 +26,7 @@ let criaPost = () => {
   posts.innerHTML += ` 
    <div class="container1">
     <div class="caixa2">
-      <h2 id="conteudo">${lembretes.text}</h2>
+      <h2 class="conteudo">${lembretes.text}</h2>
       <div class="container2">
          <button class="botao2" onClick="editaPost(this)">editar</button>
          <button class="botao3" onClick="removePost(this)">apagar</button>
@@ -38,17 +37,13 @@ let criaPost = () => {
   input.value = ''
 }
 
+var conteudo = null
+
 let removePost = e => {
   e.parentElement.parentElement.remove()
 }
 
 let editaPost = e => {
-  e.parentElement.parentElement.innerHTML += `
-  <div class="container1">
-      <div class="caixa2">
-        <textarea class="textarea" id="conteudo">${lembretes.text}</textarea>
-        <button class="botao4">Confirmar</button>
-        </div>
-      </div>
-    </div>`
+  input.value = e.parentElement.previousElementSibling.innerHTML
+  e.parentElement.parentElement.remove()
 }
